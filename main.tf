69a2cb76a779f3a682168fda959a5020a68264f5
Erick Herrera <e.herrera@accenture.com>
2025-06-10 11:57:29 -0600

    Terraform organization

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "ErickOrganization"
 
     workspaces {
       name = "gh-actions-demo"
@@ -24,7 +24,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
